fdc6c4943fb019634152a6b72efc9e1e2ca1911a
Elias Westerberg <elias.westerberg@gmail.com>
2017-01-17 23:30:57 +0100

    Scoring, arguments and random choice

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # yahtzeepy
 Yahtzee bot in Python
+
+## Overview
+* main.py: Generic python implementation of yahtzee. You can any configure for any ruleset.
+
+* generate_data.py: Generates test & training data by playing number of games and saving input & output files.
+
+* Tensorflow configuration: TBD
+
+## How to train neural nets
+* Use training data from input/keeper to train a neural net on what dice to keep
+* Use training data from input/placer to train a neural net on where to place dice
+
+
+
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 main
 """
 
+import sys, getopt
 import random
 
 # import tensorflow as tf
@@ -28,6 +29,39 @@ def dice_counts_to_dice(dice_counts):
             dice.append(i + 1)
     return dice
 
+# If no dice of the appropriate type are found you will strike = 0p
+
+def score_ones(dice_counts):
+    return 1 * dice_counts[0]
+
+def score_twos(dice_counts):
+    return 2 * dice_counts[1]
+
+def score_threes(dice_counts):
+    return 3 * dice_counts[2]
+
+def score_fours(dice_counts):
+    return 4 * dice_counts[3]
+
+def score_fives(dice_counts):
+    return 5 * dice_counts[4]
+
+def score_sixes(dice_counts):
+    return 6 * dice_counts[5]
+
+def score_type(dice_counts, type):
+    return type * dice_counts[type-1]
+
+def score_pair(dice_counts):
+    for i, x in enumerate(dice_counts):
+        if x == 2: score_type(dice_counts, i+1)
+    return 0
+
+def score_3_kind(dice_counts):
+    for i, x in enumerate(dice_counts):
+        if x == 3: score_type(dice_counts, i+1)
+    return 0
+
 class YahtzeeScoresheet:
     def __init__(self):
         self.score_names = (
@@ -49,12 +83,34 @@ class YahtzeeScoresheet:
         self.scores = range(len(self.score_names))
 
 
-
-
 class YahtzeePlayer:
 
     def __init__(self, player_index): # keep_decision_function, assign_decision_function):
         self.player_index = player_index
+        self.options = {'ones': 0, 'twos': 0, 'threes': 0, 'fours': 0, 'fives': 0, 'sixes': 0, 'pair': 0, '3-kind': 0}
+        self.score = 0
+
+    def random_assign(self, dice_counts):
+        open_options = []
+
+        # print('options: ' , str(self.options))
+        for k, v in self.options.items():
+            if v == 0:
+                open_options.append(k)
+        choice = random.choice(open_options)
+        scoring_options = {
+            'ones' : score_ones(dice_counts),
+            'twos' : score_twos(dice_counts),
+            'threes' : score_threes(dice_counts),
+            'fours' : score_fours(dice_counts),
+            'fives' : score_fives(dice_counts),
+            'sixes' : score_sixes(dice_counts),
+            'pair' : score_pair(dice_counts),
+            '3-kind' : score_3_kind(dice_counts)
+        }
+        self.options[choice] = 1
+        print('Picking', choice)
+        return scoring_options[choice]
 
     def keep_dice(self, dice_counts):
         """
@@ -66,18 +122,25 @@ class YahtzeePlayer:
     def assign_dice(self, dice_counts):
         print('testing assign for {}...'.format(dice_counts))
         print(dice_counts_to_dice(dice_counts))
+        round_score = self.random_assign(dice_counts)
+        print('scored', round_score)
+        self.score += round_score
         return True
+
 
 
 class YahtzeeGame:
 
-    def __init__(self, dice_count=5, rethrow_count=2, player_count=2, round_count=5):
+    def __init__(self, dice_count, rethrow_count, player_count, round_count, inputfile, outputfile):
         self.dice_count = dice_count
         self.rethrow_count = rethrow_count
         self.player_count = player_count
         self.round = 0
         self.players = [YahtzeePlayer(x) for x in range(player_count)]
         self.round_count = round_count
+        self.inputfile = inputfile
+        self.outputfile = outputfile
+
         print('Game started\n')
 
     def roll(self, dice_count):
@@ -122,16 +185,53 @@ class YahtzeeGame:
         self.round += 1
 
         if self.round > self.round_count:
+            for player in self.players:
+                print('player {}'.format(player.player_index + 1), ': {}'.format(player.score), 'points')
             print('Game over')
             return False
         else:
             return True
 
-dice_count = 3
-rethrow_count = 1
-player_count = 2
-round_count = 2 # 15
-game = YahtzeeGame(dice_count, rethrow_count, player_count, round_count)
 
-while game.update():
-    pass
+def main(argv):
+
+    # Defaults
+    dice_count = 3
+    rethrow_count = 1
+    player_count = 2
+    round_count = 7
+    inputfile = ''
+    outputfile = ''
+
+    try:
+        opts, args = getopt.getopt(argv,"h:d:t:p:r:i:o:",["dice=","throws=","players=","rounds=","ifile=","ofile="])
+    except getopt.GetoptError:
+        print('main.py -d <dice> -t <throws> -p <players> -r <rpunds> -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('main.py -d <dice> -t <throws> -p <players> -r <rpunds> -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-d", "--dice"):
+            dice_count = int(arg)
+        elif opt in ("-t", "--throws"):
+            rethrow_count = int(arg)
+        elif opt in ("-p", "--players"):
+            player_count = int(arg)
+        elif opt in ("-r", "--rounds"):
+            round_count = int(arg)
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+
+    print('Will start a game with', dice_count, ' dice, ', player_count, 'players.' )
+
+    game = YahtzeeGame(dice_count, rethrow_count, player_count, round_count, inputfile, outputfile)
+
+    while game.update():
+        pass
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
